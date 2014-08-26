81a7937dfe140560d140ded1116ea1c1ad2f8eb7
Renan Vicente <renanvice@gmail.com>
2014-08-26 16:50:37 -0300

    add testing

---
+++ examples/example.py
@@ -0,0 +1,11 @@
+from threadurl import ThreadUrl
+
+if __name__ == '__main__':
+  url = ['http://google.com.br','http://renanvicente.com','http://renanvicente.com.br']
+  trigger = ThreadUrl(5)
+  trigger.load()
+  trigger.method = "GET"
+  trigger.verbose = True
+  trigger.trigger(url)
+  print(trigger.data_done)
+
---
+++ tests/test_threadurl.py
@@ -0,0 +1,41 @@
+import unittest
+from threadurl import ThreadUrl
+
+class testThreadUrl(unittest.TestCase):
+
+	def setUp(self):
+		self.threadurl = ThreadUrl()
+		self.threadurl.load()
+
+	def test_url_list(self):
+		urls = ['http://linuxextreme.org','http://renanvicente.com']
+		self.threadurl.trigger(urls)
+
+	def test_url_list_from_file(self):
+		urls = open('test_file.txt','r')
+		self.threadurl.trigger(urls)
+		
+	def test_method(self):
+		urls = ['http://linuxextreme.org','http://renanvicente.com']
+		self.method = "HEAD"
+		self.threadurl.trigger(urls)
+
+	def test_verbose(self):
+		import sys
+		from StringIO import StringIO
+
+		self.threadurl.verbose = True
+		url = ['http://renanvicente.com']
+		saved_stdout = sys.stdout
+		try:
+			out = StringIO()
+			sys.stdout = out
+			self.threadurl.trigger(url)
+			output = out.getvalue().strip()
+			assert output == "(200, 'http://renanvicente.com')"
+		finally:
+			sys.stdout = saved_stdout
+
+
+if __name__ == '__main__':
+	unittest.main()
